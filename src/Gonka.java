f36649acf4a22da5c5b47c27044282a9182f5ab8
Khnaster <abrserg@list.ru>
2019-12-15 21:42:50 +0300

    Commit 11

---
+++ src/Gonka.java
@@ -0,0 +1,59 @@
+public class Gonka extends Thread {
+    static   int  a=0;
+    static   int  b=0;
+
+    public static  void main(String[] args) {
+        Thread0 thread1 = new  Thread0 ("FORD");
+        thread1.start();
+        Thread0 thread2 = new Thread0("Ferrari");
+        thread2.start();
+    }
+
+    static class  Thread0 implements Runnable {
+        Thread guruthread;
+        private String guruname;
+        Thread0(String name) {
+            guruname = name;    }
+
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(guruname + " " + i + " километр ");
+                if (guruname == "FORD") {
+                    a = i;
+                } else {
+                    b = i;
+                }
+                if (a > b & a == 9) {
+                    a=10;
+                    System.out.println("FORD победил");
+
+                }
+                if (b > a & b == 9) {
+                    b=10;
+                    System.out.println("Ferrari победил");
+
+                }
+                if (a==9 & b==10) {
+                    System.out.println("FORD проиграл!!!");
+                }
+                if (b==9 & a==10) {
+                    System.out.println("Ferrari проиграл!!!");
+                }
+            }
+
+        }
+
+        public void start() {
+            System.out.println(" Старт " + guruname);
+            if (guruthread == null) {
+                guruthread = new Thread(this, guruname);
+                guruthread.start();
+
+
+            }
+
+
+        }
+    }}
